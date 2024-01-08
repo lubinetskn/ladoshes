c252883fda285c43e8d14ba2cc93f27ab933fd8a
konstantin.lubinets <konstantin.lubinets@x5.ru>
2024-01-08 20:10:13 +0300

    omit & reduce

---
+++ omit.js
@@ -0,0 +1,43 @@
+
+  // ==============
+  // cpu - O(N*K)
+  // mem - O(N)
+  // 1 получить массив ключей
+  // 2 отфильтровать не нужное
+  // 3 добавить оставшиеся в результирующий объект
+  function omit(obj, filterKeys) {
+    const result = {};
+    for (let key in obj) {
+      if (!filterKeys.includes(key)) {
+        result[key] = obj[key];
+      }
+    }
+  
+    return result;
+  }
+  
+  // cpu - O(N + K)
+  // mem - O(N + K)
+  function omit2(obj, filterKeys) {
+    const result = {};
+    const filterKeysMap = new Set(filterKeys);
+  
+    for (let key in obj) {
+      if (!filterKeysMap.has(key)) {
+        result[key] = obj[key];
+      }
+    }
+  
+    return result;
+  }
+  
+  const obj = {
+    a: 5,
+    b: false,
+    c: 'adsf',
+  };
+  
+  console.log(omit2(obj, ['a'])); // {b: false, c: 'asdf'}
+  console.log(omit2(obj, ['b'])); // {a: 5, c: 'asdf'}
+  console.log(omit2(obj, ['b', 'c'])); // {a: 5}
+  
---
+++ reduce.js
@@ -0,0 +1,27 @@
+// reduce
+function reduce(arr, cb, init) {
+    let result = init ?? arr[0];
+    let i = arguments.length < 3 ? 1 : 0;
+  
+    for (; i < arr.length; i++) {
+      const elem = arr[i];
+      result = cb(result, elem);
+    }
+    return result;
+  }
+  
+  // console.log([1, 2, 3].reduce((acc, c) => acc + c, 0));
+  // console.log(reduce2([1, 2, 3], (acc, c) => acc + c, 0));
+  // console.log(reduce2([1, 2, 3], (acc, c) => acc + c));
+  
+  function reduce2(arr, cb, init) {
+    let [result, startIndex] =
+      typeof init === 'undefined' ? [arr[0], 1] : [init, 0];
+  
+    for (let i = startIndex; i < arr.length; i++) {
+      const elem = arr[i];
+      result = cb(result, elem, i, arr);
+    }
+    return result;
+  }
+  
